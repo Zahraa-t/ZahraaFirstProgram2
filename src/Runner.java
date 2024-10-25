dc0b9836329302cfaf9768e2648791e36ccda493
BT_1N3_19 <sample@bths.edu>
2024-10-25 13:13:44 -0400

    added strings

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("It's almost halloween!");
+        String halloween = "Halloween is on ";
+        String halloweenDate = "October 31st!";
+        System.out.println(halloween + halloweenDate);
     }
 
 }
