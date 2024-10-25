d36ef6ba3afecc673f78856dfdc88eb2cc370e3e
BT_1N3_19 <sample@bths.edu>
2024-10-25 13:16:35 -0400

    added changed print statements and added int variable

--- src/Runner.java
+++ src/Runner.java
@@ -2,9 +2,11 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("It's almost halloween!");
-        String halloween = "Halloween is on ";
-        String halloweenDate = "October 31st!";
+        String halloween = "Halloween is not in ";
+        String halloweenDate = "September!";
         System.out.println(halloween + halloweenDate);
+
+        int x = 30;
     }
 
 }
